9eff96e132cbf52931eac8dc7537f709411f7820
arslantahir34 <arslanvibesforever@gmail.com>
2025-10-20 20:07:05 +0500

    Fixed Jest open handle issue for CI workflow

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 const request = require('supertest');
 const app = require('./app');
+let server;
+
+beforeAll(() => {
+  server = app.listen(4000); // start on another port for testing
+});
+
+afterAll(() => {
+  server.close(); // stop server to avoid open handles
+});
 
 test('should respond with Hello CI from Node.js!', async () => {
-  const res = await request(app).get('/');
+  const res = await request(server).get('/');
   expect(res.text).toBe('Hello CI from Node.js!');
 });
